490ab1fefc88bce172b4cea629a242609f5065fd
0zd0 <67220210+0zd0@users.noreply.github.com>
2025-12-07 12:09:34 +0300

    feat(config): vite config path

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -3,9 +3,16 @@
  */
 export interface LunaPressConfig {
     /**
+     * Optional path to the Vite configuration file.
+     * Use this if your vite config is not in the root or named differently.
+     * @example './configs/vite.custom.ts'
+     */
+    viteConfigPath?: string
+
+    /**
      * Config for LunaPress CLI, which generates JSON with translations for .pot
      */
-    i18n: {
+    i18n?: {
         /**
          * Entry points for your application.
          * The translation extractor will start with these files and resolve the dependency graph.
